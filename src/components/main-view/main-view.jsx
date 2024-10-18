6b85eecda902ee1dbe6033d7f0ba0ee4b12d9aae
Bob3x <bob3xx@protonmail.com>
2024-10-18 20:20:28 +0300

    Fixing MainView component's structure

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,56 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Fight Club",
-            image: "https://www.imdb.com/title/tt0137523/mediaviewer/rm1412004864/?ref_=tt_ov_i",
-            genre: "Drama"
-        },
-        {
-            id: 2,
-            title: "The Lord of the Rings: The Fellowship of the Ring",
-            image: "https://www.imdb.com/title/tt0120737/mediaviewer/rm3592958976/?ref_=tt_ov_i",
-            genre: "Adventure"
-        },
-        {
-            id: 3,
-            title: "The Matrix",
-            image: "https://www.imdb.com/title/tt0133093/mediaviewer/rm525547776/?ref_=tt_ov_i",
-            genre: "Action"
-        }
+    const [movies, setMovies] = useState([]);
 
-    ])
+    useEffect(() => {
+        fetch("")
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesFromMongo = data.movies.map((movie) => {
+                return {
+                    id: movie.key,
+                    title: movie.title,
+                    genre: movie.genre,
+                    image: movie.img
+                }
+            })
+            setMovies(moviesFromMongo);
+        });
+    }, []);
+    
     const [selectedMovie, setSelectedMovie] = useState(null);
-
-    if (selectedMovie) {
-        return (<MovieView
-            movie = {selectedMovie}
-            onBackClick = {() => {
-                setSelectedMovie(null);
-            }}
-            />
-        );
-    }
-}
-if (movies.length === 0) {
-    return <div>There are no movies!</div>
-}
-return (
+    
+      
+    return (
     <div>
-        {movies.map((movie) => {
-            return ( 
+        {selectedMovie ? (
+        <MovieView
+            movie = {selectedMovie}
+            onBackClick = {() => setSelectedMovie(null) }
+            /> 
+        ): movies.length === 0 ? (
+           <div>There are no movies!</div>
+        ): ( movies.map((movie) => (
                 <MovieCard 
                 key = {movie.id}
                 movie = {movie}
-                onMovieClick = {(newSelectedMovie) => {
-                    setSelectedMovie(newSelectedMovie);
-                }}
+                onMovieClick = {(newSelectedMovie) => 
+                    setSelectedMovie(newSelectedMovie)}
                 />
-            );
-        })}
+            ))
+        )}
     </div>
 );
+}

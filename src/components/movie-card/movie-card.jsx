6ee56ad6a749ef0a022e1e6978930b8dbac36fe6
Bob3x <bob3xx@protonmail.com>
2024-10-19 16:59:20 +0300

    Adding PropTypes and properties of movie

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,10 +5,34 @@ export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
         onClick={() => {
-            onMovieClick(movie, onMovieClick);
+            onMovieClick(movie, onMovieClick)
         }} 
         >
-            {movie.title}
+        <img src = {movie.image}
+        alt = {movie.title} 
+        />
+        <h3>{movie.title}</h3>
         </div>
     );
+}
+
+MovieCard.PropTypes = {
+    movie: PropTypes.shape ({
+        _id: PropTypes.string.isRequired,
+        TItle: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }).isRequired,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string,
+            Death: PropTypes.string
+        }).isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Featured: PropTypes.bool.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 }

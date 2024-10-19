1c7423b7e851f9c80bbac66cd24b682b738210f1
Bob3x <bob3xx@protonmail.com>
2024-10-19 17:32:49 +0300

    Adding PropTypes in movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,14 @@
-const MovieView = ({movie, onBackClick}) => {
+import React from "react";
+import PropTypes from "prop-types";
+
+export const MovieView = ({movie, onBackClick}) => {
     return (
         <div>
          <div>
-            <img src={movie.image} /> 
+            <img 
+            src={movie.image} 
+            alt={movie.title}
+            /> 
          </div>
         <div>
             <span>Title: </span>
@@ -10,11 +16,40 @@ const MovieView = ({movie, onBackClick}) => {
         </div>
         <div>
             <span>Genre: </span>
-            <span>{movie.genre}</span> 
+            <span>{movie.genre.name}</span>
+            <span>{movie.genre.description}</span> 
+        </div>
+        <div>
+            <span>Description: </span>
+            <span>{movie.description}</span> 
+        </div>
+        <div>
+            <span>Director: </span>
+            <span>{movie.director.name} {movie.director.birth}</span>
+            <span>{movie.director.bio}</span> 
         </div>
         <button onClick={onBackClick}>Back</button>
         </div>
     );
 };
 
-export default MainView;
+MovieView.propTypes = {
+    movie: PropTypes.shape ({
+        _id: PropTypes.string.isRequired,
+        TItle: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }).isRequired,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string,
+            Death: PropTypes.string
+        }).isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Featured: PropTypes.bool.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};

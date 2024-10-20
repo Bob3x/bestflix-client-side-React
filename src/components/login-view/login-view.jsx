fcc77cddf427ef036bd3dd6be1a1aa39fd213844
Bob3x <bob3xx@protonmail.com>
2024-10-21 01:06:53 +0300

    Adding authorization user's data

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,58 @@
+import { useState } from "react";
+
 export const LoginView = () => {
+    const[username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    const data = {
+        Username: username,
+        Password: password
+    };
+    fetch("https://my-movies-flix-app-56f9661dc035.herokuapp.com/login", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+            onLoggedIn(data.user, data.token);
+        } else (data.user) {
+            alert("No such user");
+        }
+    })
+    .catch((e) => {
+        alert("Something went wrong");
+    })
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Username: 
-                <input type="text" />
+                <input 
+                    type="text"
+                    value={username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                    required
+                />
             </label>
             <label>
                 Password: 
-                <input type="password" />
+                <input 
+                    type="password"
+                    value ={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    required
+                />
             </label>
             <button type="submit">Submit</button>
         </form>
     );
-};
+}:

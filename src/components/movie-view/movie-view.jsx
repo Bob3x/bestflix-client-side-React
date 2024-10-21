d62151c1a1505287c37ee67d149484db0850f430
Bob3x <bob3xx@protonmail.com>
2024-10-21 23:04:53 +0300

    Fixing and reordering some movie's properties

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -15,41 +15,42 @@ export const MovieView = ({movie, onBackClick}) => {
             <span>{movie.title}</span> 
         </div>
         <div>
+            <span>Description: </span>
+            <span>{movie.description}</span> 
+        </div>
+        <div>
             <span>Genre: </span>
             <span>{movie.genre.name}</span>
             <span>{movie.genre.description}</span> 
         </div>
         <div>
-            <span>Description: </span>
-            <span>{movie.description}</span> 
-        </div>
-        <div>
             <span>Director: </span>
-            <span>{movie.director.name} {movie.director.birth}</span>
+            <span>{movie.director.name}</span>
+            <span>{movie.director.birth}</span>
             <span>{movie.director.bio}</span> 
         </div>
         <button onClick={onBackClick}>Back</button>
-        </div>
+    </div>
     );
 };
 
 MovieView.propTypes = {
     movie: PropTypes.shape ({
         _id: PropTypes.string.isRequired,
-        TItle: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        Genre: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Description: PropTypes.string.isRequired
+        tItle: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        genre: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired
         }).isRequired,
-        Director: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Bio: PropTypes.string.isRequired,
-            Birth: PropTypes.string,
-            Death: PropTypes.string
+        director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bio: PropTypes.string.isRequired,
+            birth: PropTypes.string,
+            death: PropTypes.string
         }).isRequired,
-        ImagePath: PropTypes.string.isRequired,
-        Featured: PropTypes.bool.isRequired
+        image: PropTypes.string.isRequired,
+        featured: PropTypes.bool.isRequired
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    onBackClick: PropTypes.func.isRequired
 };

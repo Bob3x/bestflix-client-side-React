4e5eaf5eab29b332694977da87ea5fd4e1538790
Bob3x <bob3xx@protonmail.com>
2024-10-22 00:07:05 +0300

    Updating form's fields and adding fetch function

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 export const SignupView = () => {
-    const handleSubmit = (event) => {};
+    const [ username, setUsername ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ birthday, setBirthday] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            Username: username,
+            Password: password, 
+            Email: email,
+            Birthday: birthday
+        }
+
+        fetch("SIGNUP_URL", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            if (response.ok) {
+                alert("signup successful");
+                window.location.reload();
+            } else {
+                alert("Singup failed");
+            }
+        });
+    };
     return (
         <form onSubmit ={handleSubmit}>
             <button type="submit">Submit</button>

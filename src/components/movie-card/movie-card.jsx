b9e32612a87c1e25d8a2fe7088f19af50a49cadd
Bob3x <bob3xx@protonmail.com>
2024-11-01 20:32:18 +0200

    Using Link and defing path

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,38 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-        <Card>
-            <Card.Img variant="top" src = {movie.image} alt = {movie.title} />
-            <Card.Body>
-                <Card.Title>{movie.title}</Card.Title>
-                <Card.Text>{movie.description}</Card.Text>
-                <Button onClick={() => {onMovieClick(movie, onMovieClick)}} variant="link">
-                    Open</Button> 
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card>
+      <Card.Img variant="top" src={movie.image} alt={movie.title} />
+      <Card.Body>
+        <Card.Title>
+          <strong>{movie.title}</strong>
+        </Card.Title>
+        <Card.Text>
+          <p>Genre: {movie.genre.name}</p>
+          {movie.description}
+        </Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
 };
 
 MovieCard.propTypes = {
-    movie: PropTypes.shape ({
-        _id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        genre: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired
-        }).isRequired,
-        director: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            bio: PropTypes.string.isRequired,
-            birth: PropTypes.string,
-            death: PropTypes.string
-        }).isRequired,
-        image: PropTypes.string.isRequired,
-        featured: PropTypes.bool.isRequired
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      birth: PropTypes.string,
+      death: PropTypes.string,
+    }).isRequired,
+    image: PropTypes.string.isRequired,
+    featured: PropTypes.bool.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };

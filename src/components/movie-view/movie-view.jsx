9935d5a28fcf03cba4cf7427191a8f93c5cfe528
Bob3x <bob3xx@protonmail.com>
2024-11-01 20:27:43 +0200

    Using Link component

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,61 +1,68 @@
 import React from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import { Container, Row, Col, Card, Button } from "react-bootstrap"
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
-export const MovieView = ({movie, onBackClick}) => {
-    return (
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
+
+  const movie = movies.find((m) => m._id === movieId);
+
+  return (
     <Container>
-        <Row>
+      <Row>
         <Col md={5}>
-        <Card className="mt-3 mb-2" >
-            <Card.Img src={movie.image} alt={movie.title}  /> 
-          <Card.Body>
-            <Card.Header>
-            <Card.Title><strong>{movie.title}</strong></Card.Title>
-            </Card.Header>
-            <Card.Text>
+          <Card className="mt-3 mb-2">
+            <Card.Img src={movie.image} alt={movie.title} />
+            <Card.Body>
+              <Card.Header>
+                <Card.Title>
+                  <strong>{movie.title}</strong>
+                </Card.Title>
+              </Card.Header>
+              <Card.Text>
                 <h5>Genre:</h5>
-                   <p>{movie.genre.name}</p>
+                <p>{movie.genre.name}</p>
                 <h5>Description:</h5>
                 <p>{movie.description}</p>
-            <h5>Director: </h5>
+                <h5>Director: </h5>
                 <span>{movie.director.name}</span>
-            <p>Birth year: {movie.director.birth}</p>
-            <h5>Bio: </h5>
-                <p>{movie.director.bio}</p> 
-                </Card.Text>
-            
-            
-        
-        <Button 
-            onClick={onBackClick}
-            variant="secondary"
-            >Back</Button>
-    </Card.Body>        
-    </Card>
-    </Col>
-    </Row>
+                <p>Birth year: {movie.director.birth}</p>
+                <h5>Bio: </h5>
+                <p>{movie.director.bio}</p>
+              </Card.Text>
+
+              <Link to={`/`}>
+                <Button onClick={onBackClick} variant="secondary">
+                  Back
+                </Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
-    );
+  );
 };
 
 MovieView.propTypes = {
-    movie: PropTypes.shape ({
-        _id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        genre: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired
-        }).isRequired,
-        director: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            bio: PropTypes.string.isRequired,
-            birth: PropTypes.string,
-            death: PropTypes.string
-        }).isRequired,
-        image: PropTypes.string.isRequired,
-        featured: PropTypes.bool.isRequired
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
     }).isRequired,
-    onBackClick: PropTypes.func.isRequired
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      birth: PropTypes.string,
+      death: PropTypes.string,
+    }).isRequired,
+    image: PropTypes.string.isRequired,
+    featured: PropTypes.bool.isRequired,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 };

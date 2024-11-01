51b04c666f312fc10fc8e424c25fec6d5a2fa633
Bob3x <bob3xx@protonmail.com>
2024-11-01 23:20:09 +0200

    Developing profile view functionality

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -31,5 +31,6 @@ return (
   <div>
     <UserInfo name={user.Username} email={user.Email} />
     <FavoriteMovies favoriteMovieList={favoriteMovieList} />
+    <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
   </div>
 );

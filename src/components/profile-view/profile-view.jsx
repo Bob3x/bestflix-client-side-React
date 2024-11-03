aefea9a08593567dc6c077611cab3a4c9e52b42b
Bob3x <bob3xx@protonmail.com>
2024-11-03 03:32:29 +0200

    Updating profile view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,36 +1,43 @@
 import React from "react";
-import UserInfo from "./user-info";
-import FavoriteMovies from "./favorite-movies";
+import { UserInfo } from "./user-info";
+import { UpdateUser } from "./update-user";
+import { FavoriteMovies } from "./favorite-movies";
 
-export const ProfileView = ({ movies }) => {
-  const favoriteMovieList = movies.filter((movies) => {});
-
-  const ProfileDelete = () => {
+export const ProfileView = ({ user, token, updatedUser, onLoggedOut }) => {
+  const UserRemove = () => {
     fetch(
       "https://my-movies-flix-app-56f9661dc035.herokuapp.com/users/${user.Username}",
       {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
-          Authorizaton: "Bearer $(token)",
+          Authorization: `Bearer ${token}`,
         },
       }
     ).then((response) => {
       console.log(response);
       if (response.ok) {
         console.log("Account deleted succesfully!");
-        onLoggedOyt();
+        onLoggedOut();
       } else {
         alert("Failed to delete account");
       }
     });
   };
-};
 
-return (
-  <div>
-    <UserInfo name={user.Username} email={user.Email} />
-    <FavoriteMovies favoriteMovieList={favoriteMovieList} />
-    <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
-  </div>
-);
+  return (
+    <div>
+      <UserInfo name={user.Username} email={user.Email} />
+      <FavoriteMovies favoriteMovieList={favoriteMovieList} />
+      <UpdateUser user={user} token={token} userUpdate={userUpdate} />
+      <Button
+        variant="danger"
+        onClick={() => {
+          UserRemove();
+        }}
+      >
+        Delete account
+      </Button>
+    </div>
+  );
+};

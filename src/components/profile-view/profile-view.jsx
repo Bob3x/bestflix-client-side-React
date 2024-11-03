57cd62dd832bde02633972f1ed4fd836163b62cd
Bob3x <bob3xx@protonmail.com>
2024-11-03 22:59:43 +0200

    Fixing handle remove function and dynamic URL

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -3,41 +3,33 @@ import { UserInfo } from "./user-info";
 import { UpdateUser } from "./update-user";
 import { FavoriteMovies } from "./favorite-movies";
 
-export const ProfileView = ({ user, token, updatedUser, onLoggedOut }) => {
-  const UserRemove = () => {
-    fetch(
-      "https://my-movies-flix-app-56f9661dc035.herokuapp.com/users/${user.Username}",
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    ).then((response) => {
-      console.log(response);
-      if (response.ok) {
-        console.log("Account deleted succesfully!");
-        onLoggedOut();
-      } else {
-        alert("Failed to delete account");
-      }
-    });
-  };
+export const ProfileView = ({ user, token, onLoggedOut }) => {
+    const handleUserRemove = () => {
+        fetch(`https://my-movies-flix-app-56f9661dc035.herokuapp.com/users/${user.Username}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((response) => {
+            console.log(response);
+            if (response.ok) {
+                console.log("Account deleted succesfully!");
+                onLoggedOut();
+            } else {
+                alert("Failed to delete account");
+            }
+        });
+    };
 
-  return (
-    <div>
-      <UserInfo name={user.Username} email={user.Email} />
-      <FavoriteMovies favoriteMovieList={favoriteMovieList} />
-      <UpdateUser user={user} token={token} userUpdate={userUpdate} />
-      <Button
-        variant="danger"
-        onClick={() => {
-          UserRemove();
-        }}
-      >
-        Delete account
-      </Button>
-    </div>
-  );
+    return (
+        <div>
+            <UserInfo name={user.Username} email={user.Email} />
+            <FavoriteMovies favoriteMoviesList={favoriteMoviesList} />
+            <UpdateUser user={user} token={token} onLoggedOut={onLoggedOut} />
+            <Button variant="danger" onClick={handleUserRemove}>
+                Delete account
+            </Button>
+        </div>
+    );
 };

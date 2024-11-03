21f2b9c760161c56ba2e62818a9f87fa1aa22c12
Bob3x <bob3xx@protonmail.com>
2024-11-03 22:57:46 +0200

    Fixing favorite movies function

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -4,24 +4,34 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const FavoriteMovies = ({ favoriteMovieList }) => {
-  return (
-    <div>
-      favoriteMovieList.length === 0 ? (<p>No favorite movies</p>) : (
-      <h2>Favorite Movies</h2>
-      {favoriteMovieList.map((movie) => (
-        <div key={movie._id}>
-          <img src={movie.image} />
-          <Link to={"/movies/${movies._id"}>
-            <h4>{movie.title}</h4>
-            <Button variant="secondary">Movie Info</Button>
-          </Link>
+    return (
+        <div>
+            {favoriteMovieList.length === 0 ? (
+                <p>No favorite movies</p>
+            ) : (
+                <>
+                    <h2>Favorite Movies</h2>
+                    {favoriteMovieList.map((movie) => (
+                        <div key={movie._id}>
+                            <img src={movie.image} alt={movie.title} className="img-fluid" />
+                            <Link to={`/movies/${movie._id}`}>
+                                <h4>{movie.title}</h4>
+                                <Button variant="secondary">Movie Info</Button>
+                            </Link>
+                        </div>
+                    ))}
+                </>
+            )}
         </div>
-      ))}
-      )
-    </div>
-  );
+    );
 };
 
-FavoriteMovies.prototype = {
-  FavoriteMovies: PropTypes.array.isRequired,
+FavoriteMovies.propTypes = {
+    favoriteMovieList: PropTypes.arrayOf(
+        PropTypes.shape({
+            _id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            image: PropTypes.string.isRequired,
+        })
+    ).isRequired,
 };

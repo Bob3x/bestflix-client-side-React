e50b0422484b8732d1c1448889455934796bcaee
Bob3x <bob3xx@protonmail.com>
2024-11-12 00:40:42 +0200

    Update component

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,25 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
 import { Card } from "react-bootstrap";
 
-export const UserInfo = ({ name, email }) => {
+export const UserInfo = ({ user, email }) => {
     return (
         <Card className="mb-4">
             <Card.Header>
                 <Card.Title>User Information</Card.Title>
             </Card.Header>
             <Card.Body>
-                <Card.Text
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                    }}
-                >
-                    <strong>Name:</strong> {name}
+                <Card.Text>
+                    <span>Name:</span> {user}
                 </Card.Text>
                 <Card.Text>
-                    <strong>Email:</strong> {email}
+                    <span>Email:</span> {email}
                 </Card.Text>
             </Card.Body>
         </Card>
     );
+};
+
+UserInfo.propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired
 };

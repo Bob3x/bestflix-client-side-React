b6020580061aac6cfafa00e2541e636880a711e4
Bob3x <bob3xx@protonmail.com>
2024-11-11 05:29:23 +0200

    Update user information view

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,8 +1,25 @@
+import { Card } from "react-bootstrap";
+
 export const UserInfo = ({ name, email }) => {
-  return (
-    <>
-      <p>User: {name}</p>
-      <p>Email: {email}</p>
-    </>
-  );
+    return (
+        <Card className="mb-4">
+            <Card.Header>
+                <Card.Title>User Information</Card.Title>
+            </Card.Header>
+            <Card.Body>
+                <Card.Text
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    <strong>Name:</strong> {name}
+                </Card.Text>
+                <Card.Text>
+                    <strong>Email:</strong> {email}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
 };

ffc74aabe28671287af3cc881b0338c8cfe3233a
Bob3x <bob3xx@protonmail.com>
2024-11-13 21:14:30 +0200

    Fixing and simplifing search-bar component

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -1,66 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export const SearchBar = ({ moviesAPI, onFilter }) => {
-    const [searchInput, setSearchInput] = useState("");
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        console.log("Movies API Data:", moviesAPI);
-
-        const searchTerm = searchInput.toLowerCase().trim();
-        console.log("Search Input:", searchInput);
-        try {
-            const filteredMovies = moviesAPI.filter((movie) => {
-                const titleMatch = movie.title?.toLowerCase().includes(searchTerm);
-                const descMatch = movie.description?.toLowerCase().includes(searchTerm);
-                const genreMatch = movie.genre.name?.toLowerCase().includes(searchTerm);
-                const directorMatch = movie.director.name?.toLowerCase().includes(searchTerm);
-
-                // Return true if any field matches
-                return titleMatch || descMatch || genreMatch || directorMatch;
-            });
-
-            console.log("Filtered Movies:", filteredMovies.length);
-            onFilter(filteredMovies);
-        } catch (error) {
-            console.error("Movie not found", error);
-        }
-    };
-
-    const handleChange = (e) => {
-        setSearchInput(e.target.value);
-    };
-
+export const SearchBar = ({ searchQuery, onSearch }) => {
     return (
-        <form onSubmit={handleSubmit}>
+        <form>
             <input
                 type="text"
                 placeholder="Search here"
-                onChange={handleChange}
-                value={searchInput}
+                value={searchQuery}
+                onChange={(e) => onSearch(e.target.value)}
+                className="form-control"
             />
-            <button type="submit">Search</button>
         </form>
     );
 };
 
 SearchBar.propTypes = {
-    movies: PropTypes.arrayOf(
-        PropTypes.shape({
-            _id: PropTypes.string.isRequired,
-            title: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired,
-            genre: PropTypes.shape({
-                name: PropTypes.string.isRequired,
-                description: PropTypes.string.isRequired,
-            }).isRequired,
-            image: PropTypes.string.isRequired,
-            featured: PropTypes.bool.isRequired,
-        })
-    ).isRequired,
-    moviesAPI: PropTypes.array,
-    filteredMovies: PropTypes.array,
-    onFilter: PropTypes.func.isRequired,
+    searchQuery: PropTypes.string.isRequired,
+    onSearch: PropTypes.func.isRequired
 };
